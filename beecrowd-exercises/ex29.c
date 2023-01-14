3c9ec3343695c4791bb7d22e5b5f928d4f5f50b0
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-01-14 11:16:16 -0300

    💥 fix: code correction

--- beecrowd-exercises/ex29.c
+++ beecrowd-exercises/ex29.c
@@ -3,26 +3,26 @@
 int main()
 {
     float N1,N2,N3,N4;
-    float media,exame,total;
+    float media,exame,mediafinal;
     
-    scanf("%f%f%f%f",&N1,&N2,&N3,&N4);
+            scanf("%f%f%f%f",&N1,&N2,&N3,&N4);
 
     media = ((N1*2) + (N2*3) + (N3*4) + (N4*1)) / (2+3+4+1);
 
-    if (media >= 5.0 && media <= 6.9){
+    if (media >= 5.0 || media <= 6.9){
             printf("Media: %.1f\n",media);
             printf("Aluno em exame.\n");
             scanf("%f",&exame);
             printf("Nota do exame: %.1f\n",exame);
 
-            total = (exame + media)/2;       
+            mediafinal = (exame + media)/2;       
     
-    if (total >= 5.0){   
+    if (mediafinal >= 5.0){   
             printf("aluno aprovado\n"); 
-            printf("Media final: %.1f\n",total);    
+            printf("Media final: %.1f\n",mediafinal);    
     }else{
             printf("Aluno reprovado\n");
-            printf("Media final: %.1f\n",total);      
+            printf("Media final: %.1f\n",mediafinal);      
     }
     }
     
